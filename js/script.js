7c11ccc8090191a859e27f50a2f5471e14f48fe8
RFS <97369519+rfs85@users.noreply.github.com>
2023-01-12 22:53:37 +0000

    ddd

--- js/script.js
+++ js/script.js
@@ -40,7 +40,7 @@ new Vue({
             data: {
                 language: "en",
                 theme: "default",
-                title: "my name is Arturs",
+                title: "my name is RFS",
                 subtitle: "I am GitHub Readme Generator's creator",
                 text: "I made this project just for fun, it allows you to create nice and simple GitHub Readme files that you can copy/paste and use in your profile.",
 
